c5bc90e1cc084c8915eb09b30f2f78b71d99dac7
Jon Galloway <jongalloway@gmail.com>
2016-07-31 15:12:42 +1000

    Added Lab 6 code

---
+++ LabCode/Lab6/src/Lab6/RepeatTagHelper.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Razor.TagHelpers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Lab6
+{
+    public class RepeatTagHelper: TagHelper
+    {
+        public int Count { get; set; }
+
+        public async override Task ProcessAsync(TagHelperContext context, TagHelperOutput output)
+        {
+            output.TagName = null;
+
+            for (int i = 0; i < Count; i++)
+            {
+                output.Content.AppendHtml(await output.GetChildContentAsync(useCachedResult: false));
+            }
+        }
+    }
+}
