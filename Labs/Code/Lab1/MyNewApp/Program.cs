8fc01683934b4a2fb2f0855f9ef621a81e402b06
Jon Galloway <jon.galloway@microsoft.com>
2017-01-12 17:02:13 -0800

    Updates for Lab 1

--- Labs/Code/Lab1/MyNewApp/Program.cs
+++ Labs/Code/Lab1/MyNewApp/Program.cs
@@ -1,20 +1,17 @@
-﻿using System;
+using System;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Hosting;
 
-namespace ConsoleApplication
+class Program
 {
-    public class Program
+    static void Main(string[] args)
     {
-        public static void Main(string[] args)
-        {
-            var host = new WebHostBuilder()
-                .UseKestrel()
-                .Configure(app => app.Run(context => context.Response.WriteAsync("Hello World!")))
-                .Build();
+       var host = new WebHostBuilder()
+           .UseKestrel()
+           .Configure(app => app.Run(context => context.Response.WriteAsync("Hello World!")))
+           .Build();
 
-            host.Run();
-        }
+       host.Run();
     }
 }
