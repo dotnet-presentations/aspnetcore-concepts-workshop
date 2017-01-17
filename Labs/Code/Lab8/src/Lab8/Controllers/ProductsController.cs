4da59cdb6b31b8b94a43c5f5db434e1088a7f918
David Fowler <davidfowl@gmail.com>
2017-01-17 00:06:07 +0000

    Added Lab8 code

---
+++ Labs/Code/Lab8/src/Lab8/Controllers/ProductsController.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Lab8.Models;
+
+namespace Lab8.Controllers
+{
+    [Route("/api/[controller]")]
+    [Produces("application/json")]
+    public class ProductsController : ControllerBase
+    {
+        private static List<Product> _products = new List<Product>(new[] {
+            new Product() { Id = 1, Name = "Computer" },
+            new Product() { Id = 2, Name = "Radio" },
+            new Product() { Id = 3, Name = "Apple" },
+        });
+
+        [HttpGet]
+        public IEnumerable<Product> Get() => _products;
+
+        [HttpGet("{id}")]
+        public IActionResult Get(int id)
+        {
+            var product = _products.SingleOrDefault(p => p.Id == id);
+
+            if (product == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(product);
+        }
+
+        [HttpPost]
+        public IActionResult Post([FromBody]Product product)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            _products.Add(product);
+            return CreatedAtAction(nameof(Get), new { id = product.Id }, product);
+        }
+    }
+}
---
+++ Labs/Code/Lab8/src/Lab8/Models/Product.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Lab8.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+    }
+}
