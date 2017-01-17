9da912ffe61a7155aab7039999e3d551dbadae81
Jon Galloway <jongalloway@gmail.com>
2017-01-17 11:22:34 +0000

    Updated Lab 7 doc, added code

---
+++ Labs/Code/Lab7/webpack.config.js
@@ -0,0 +1,60 @@
+var isDevBuild = process.argv.indexOf('--env.prod') < 0;
+var path = require('path');
+var webpack = require('webpack');
+var nodeExternals = require('webpack-node-externals');
+var merge = require('webpack-merge');
+var allFilenamesExceptJavaScript = /\.(?!js(\?|$))([^.]+(\?|$))/;
+
+// Configuration in common to both client-side and server-side bundles
+var sharedConfig = {
+    resolve: { extensions: [ '', '.js', '.ts' ] },
+    output: {
+        filename: '[name].js',
+        publicPath: '/dist/' // Webpack dev middleware, if enabled, handles requests for this URL prefix
+    },
+    module: {
+        loaders: [
+            { test: /\.ts$/, include: /ClientApp/, loader: 'ts', query: { silent: true } },
+            { test: /\.html$/, loader: 'raw' },
+            { test: /\.css$/, loader: 'to-string!css' },
+            { test: /\.(png|jpg|jpeg|gif|svg)$/, loader: 'url', query: { limit: 25000 } }
+        ]
+    }
+};
+
+// Configuration for client-side bundle suitable for running in browsers
+var clientBundleOutputDir = './wwwroot/dist';
+var clientBundleConfig = merge(sharedConfig, {
+    entry: { 'main-client': './ClientApp/boot-client.ts' },
+    output: { path: path.join(__dirname, clientBundleOutputDir) },
+    plugins: [
+        new webpack.DllReferencePlugin({
+            context: __dirname,
+            manifest: require('./wwwroot/dist/vendor-manifest.json')
+        })
+    ].concat(isDevBuild ? [
+        // Plugins that apply in development builds only
+        new webpack.SourceMapDevToolPlugin({
+            filename: '[file].map', // Remove this line if you prefer inline source maps
+            moduleFilenameTemplate: path.relative(clientBundleOutputDir, '[resourcePath]') // Point sourcemap entries to the original file locations on disk
+        })
+    ] : [
+        // Plugins that apply in production builds only
+        new webpack.optimize.OccurenceOrderPlugin(),
+        new webpack.optimize.UglifyJsPlugin()
+    ])
+});
+
+// Configuration for server-side (prerendering) bundle suitable for running in Node
+var serverBundleConfig = merge(sharedConfig, {
+    entry: { 'main-server': './ClientApp/boot-server.ts' },
+    output: {
+        libraryTarget: 'commonjs',
+        path: path.join(__dirname, './ClientApp/dist')
+    },
+    target: 'node',
+    devtool: 'inline-source-map',
+    externals: [nodeExternals({ whitelist: [allFilenamesExceptJavaScript] })] // Don't bundle .js files from node_modules
+});
+
+module.exports = [clientBundleConfig, serverBundleConfig];
