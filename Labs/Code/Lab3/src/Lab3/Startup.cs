d437aecf1b479f3ec7ecf43f2e17761cb1b7dd57
Arnaud Maichac <arnaud.maichac@outlook.com>
2019-05-10 22:36:26 +0200

    Update Lab 3

--- Labs/Code/Lab3/src/Lab3/Startup.cs
+++ Labs/Code/Lab3/src/Lab3/Startup.cs
@@ -3,7 +3,6 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Logging;
 
 namespace Lab3
 {
@@ -23,10 +22,8 @@ namespace Lab3
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
-            loggerFactory.AddConsole();
-
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
