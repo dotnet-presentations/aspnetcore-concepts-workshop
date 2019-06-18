48dc8da7c0b6ac7b5e229638f9b251e8ef64bd95
Jasmine <paladique@users.noreply.github.com>
2019-06-18 16:00:41 -0400

    update lab 4 and set completed code name to lab4

--- Labs/Code/Lab4/src/Lab4/Program.cs
+++ Labs/Code/Lab4/src/Lab4/Program.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
 
-namespace Lab5
+namespace Lab4
 {
     public class Program
     {
--- Labs/Code/Lab4/src/Lab4/Startup.cs
+++ Labs/Code/Lab4/src/Lab4/Startup.cs
@@ -3,7 +3,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 
-namespace Lab5
+namespace Lab4
 {
     public class Startup
     {
