a63d2786ac7abb236bd11692fad23ac4ede40035
Scott Hanselman <scott@hanselman.com>
2020-10-14 17:27:56 -0700

    updating to 3.1 LTS

--- Labs/Code/App/AttendeeList/Startup.cs
+++ Labs/Code/App/AttendeeList/Startup.cs
@@ -1,5 +1,6 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Hosting;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
@@ -30,7 +31,7 @@ namespace AttendeeList
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
             {
